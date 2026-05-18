a4c9927d0ff4aebf7beecad7c755323becbc28b6
ivchoubg <ivchobg346@gmail.com>
2026-05-18 21:13:47 +0300

    Update bot activity message to Bulgarian

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: " ",
+        name: "Използвай /help за команди!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
