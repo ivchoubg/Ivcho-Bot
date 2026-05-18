7deca45ac38e2a50f61d962ac2af97e91c5728bc
ivchoubg <ivchobg346@gmail.com>
2026-05-18 21:14:29 +0300

    Change activity name from Bulgarian to English

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Използвай /help за команди!",
+        name: "/help for commands",
         // Activity type number (0 = Playing).
         type: 0, 
       },
